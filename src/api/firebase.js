9400b73f16fe7f39cfe51b41aff4ce039ee785d2
Ma. Pauline Joy Pama <pjpama6@gmail.com>
2021-02-11 15:01:07 +0800

    Fix: firebase env config

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -3,24 +3,14 @@ import "firebase/analytics";
 import "firebase/storage";
 import "firebase/firestore";
 
-// const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_API_KEY,
-// 	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-// 	projectId: process.env.REACT_APP_PROJECT_ID,
-// 	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-// 	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-// 	appId: process.env.REACT_APP_APP_ID,
-// 	measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-// };
-
 const firebaseConfig = {
-    apiKey: "AIzaSyBZUPRHJEiwBxTs4GCszUFr8VFCpmdM4dU",
-    authDomain: "weverse-web.firebaseapp.com",
-    projectId: "weverse-web",
-    storageBucket: "weverse-web.appspot.com",
-    messagingSenderId: "539644177452",
-    appId: "1:539644177452:web:ea1070f225f6d729da3589",
-    measurementId: "G-N603WH6E8Q",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
