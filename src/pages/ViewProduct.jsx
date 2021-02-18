4c9778de5b605389718d97b42ab72641da71498c
Ma. Pauline Joy Pama <pjpama6@gmail.com>
2021-02-18 21:14:10 +0800

    Feat: update view product page content

--- src/pages/ViewProduct.jsx
+++ src/pages/ViewProduct.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Button } from "@material-ui/core";
+import { Button, Divider } from "@material-ui/core";
 import styled from "styled-components";
 import { useParams } from "react-router-dom";
 import { useDataDispatch, useDataState } from "../context/DataContext";
@@ -7,6 +7,7 @@ import Layout from "../components/shared/Layout";
 import Spinner from "../components/shared/Spinner";
 import Categories from "../components/shared/Categories";
 import { getPoints } from "../utils";
+import moment from "moment";
 
 const Text = styled.div`
     font-size: ${(props) => props.fontSize || 14}px;
@@ -45,11 +46,11 @@ const Bullet = styled.div`
 `;
 
 const Circle = styled.div`
-    color: #878e96;
+    color: red;
     margin: 20px 5px 0px 5px;
-    background: #878e96;
-    width: 4px;
-    height: 4px;
+    background: red;
+    width: 3px;
+    height: 3px;
     border-radius: 50%;
     display: inline-table;
 `;
@@ -74,19 +75,21 @@ const ButtonGroup = styled.div`
     display: flex;
 `;
 
+const DividerWrapper = styled(Divider)`
+    margin: 10px 0px 10px 30px;
+`;
+
 const ViewProduct = () => {
-    const { productLoading, product } = useDataState();
+    const { productLoading, product, shop } = useDataState();
     const { fetchProduct } = useDataDispatch();
     const { id } = useParams();
-    console.log("----", id);
-
     useEffect(() => {
         fetchProduct(id);
     }, []);
 
     return (
         <Layout>
-            {productLoading ? (
+            {!shop || productLoading ? (
                 <Spinner />
             ) : (
                 <Body>
@@ -126,28 +129,110 @@ const ViewProduct = () => {
                                         Buy Now
                                     </ButtonWrapper>
                                 </ButtonGroup>
+                                {product.isPreorder && (
+                                    <>
+                                        <DividerWrapper />
+                                        <Text
+                                            fontSize={14}
+                                            margin="10px 0px 10px 30px"
+                                        >
+                                            <img
+                                                src="/images/timer.svg"
+                                                width={10}
+                                            />{" "}
+                                            For pre-order
+                                        </Text>
+                                        <Text
+                                            fontSize={14}
+                                            margin="10px 0px 10px 30px"
+                                            color="#878E96"
+                                        >
+                                            Shipping starts on{" "}
+                                            {moment(
+                                                product.shippingDate
+                                            ).format("DD/MM/YYYY")}
+                                        </Text>
+                                    </>
+                                )}
                             </div>
                         </Details>
                         <Notice>
+                            {product.isPreorder && shop.name === "Global" && (
+                                <Bullet>
+                                    <Circle />
+                                    <Text fontSize={16} color="#878E96">
+                                        Pre-order:{" "}
+                                        {moment(
+                                            product.preorderStartDate
+                                        ).format("D MMMM, h A")}{" "}
+                                        ~{" "}
+                                        {moment(product.preorderEndDate).format(
+                                            "D MMMM, h A"
+                                        )}{" "}
+                                        (KST)
+                                    </Text>
+                                </Bullet>
+                            )}
+                            {product.preorderGift && shop.name === "Global" && (
+                                <Bullet>
+                                    <Circle />
+                                    <Text fontSize={16} color="#878E96">
+                                        {product.name} comes with{" "}
+                                        {product.preorderGift.join(" and ")}.
+                                    </Text>
+                                </Bullet>
+                            )}
+                            {product.category === "ALBUM" &&
+                                shop.name === "Global" && (
+                                    <>
+                                        <Bullet>
+                                            <Circle />
+                                            <Text fontSize={16} color="#878E96">
+                                                All sales from Weverse Shop
+                                                Global will count for the album
+                                                charts in South Korea
+                                            </Text>
+                                        </Bullet>
+                                        <Bullet>
+                                            <Circle />
+                                            <Text fontSize={16} color="#878E96">
+                                                Please visit Weverse Shop USA if
+                                                you are a customer in the US. US
+                                                customers cannot purchase this.
+                                            </Text>
+                                        </Bullet>
+                                        <Bullet>
+                                            <Circle />
+                                            <Text fontSize={16} color="#878E96">
+                                                This album will be shipped out
+                                                from Korea. The international
+                                                shipping fee will be charged
+                                                separately.
+                                            </Text>
+                                        </Bullet>
+                                    </>
+                                )}
                             <Bullet>
                                 <Circle />
-                                <Text fontSize={16} color="#BDC0C5">
+                                <Text fontSize={16} color="#878E96">
                                     You can buy up to {product.maxQtyPerOrder}.
                                 </Text>
                             </Bullet>
-                            <Bullet>
-                                <Circle />
-                                <Text fontSize={16} color="#BDC0C5">
-                                    As more countries/regions are experiencing
-                                    shipping delays and imposing shipping
-                                    restrictions due to COVID-19, shipping to
-                                    the country/region you selected may be
-                                    unavailable. Please check the announcement
-                                    on shipping delay and restriction. We will
-                                    do our best to resume shipping as soon as
-                                    possible
-                                </Text>
-                            </Bullet>
+                            {shop.name === "Global" && (
+                                <Bullet>
+                                    <Circle />
+                                    <Text fontSize={16} color="#878E96">
+                                        As more countries/regions are
+                                        experiencing shipping delays and
+                                        imposing shipping restrictions due to
+                                        COVID-19, shipping to the country/region
+                                        you selected may be unavailable. Please
+                                        check the announcement on shipping delay
+                                        and restriction. We will do our best to
+                                        resume shipping as soon as possible
+                                    </Text>
+                                </Bullet>
+                            )}
                         </Notice>
 
                         <Text fontSize={24} margin="10px 30px 30px">
