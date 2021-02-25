d0883dc5ac4ccae1a86f5ad63be672279c449fa5
Ma. Pauline Joy Pama <pjpama6@gmail.com>
2021-02-25 22:36:40 +0800

    Feat: add function to get total price

--- src/utils/index.js
+++ src/utils/index.js
@@ -12,3 +12,12 @@ export const getPoints = (amount) => Math.round(amount / 5000) * 50;
 
 export const getCartProductCount = (cart) =>
     cart.reduce((a, b) => Number(a) + Number(b.qty), 0);
+
+export const getSubtotal = (cart, selected) =>
+    cart.reduce(
+        (a, b) =>
+            selected.includes(b.id)
+                ? Number(a) + Number(b.qty * b.product.price)
+                : a,
+        0
+    );
